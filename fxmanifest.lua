35bd81298ea8fcd28ebefdc044b3ec11a9fe4281
YoDMVMoe <mmbennett1989@gmail.com>
2026-07-29 08:28:04 -0400

    fix: add missing game declaration in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,7 @@
 fx_version 'cerulean'
 use_experimental_fxv2_oal 'yes'
 lua54 'yes'
+game "rdr3"
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 name 'ox_inventory'
 author 'Overextended'
